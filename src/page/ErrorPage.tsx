89a56668650d79d99a066a8f0c8fc3bf0ef3b30e
Sachin <010sssachin@gmail.com>
2025-04-11 17:33:55 +0530

    add 404 page

---
+++ src/page/ErrorPage.tsx
@@ -0,0 +1,47 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="container">
+    <div className="trey">
+      <div className="top">
+        <div className="paper1"></div>
+      </div>
+      <div className="bottom">
+        <div className="inner"></div>
+      </div>
+      <div className="grey-border"></div>
+      <div className="pointer"></div>
+      <div className="paper2">
+        <div className="e1"></div>
+        <div className="e2"></div>
+        <div className="e4">
+          <div className="e41"></div>
+          <div className="text">ERROR</div>
+          <div className="e42"></div>
+        </div>
+        <div className="e5">
+          <div className="e51"></div>
+          <div className="text">404</div>
+          <div className="e52"></div>
+        </div>
+        <div className="e3"></div>
+        <div className="e6"></div>
+      </div>
+    </div>
+
+    <div className="body">
+      <div className="top-perspective">
+        <div className="top"></div>
+      </div>
+      <div className="bottom"></div>
+      <div className="buttons-container">
+        <div className="button1"></div>
+        <div className="button2"></div>
+      </div>
+      <div className="button3"></div>
+    </div>
+  </div>
+  );
+};
+
+export default ErrorPage;
