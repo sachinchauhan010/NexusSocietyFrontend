bb0951474f9a1f60c412bf025af0c55598e32d3c
Sachin <010sssachin@gmail.com>
2025-04-11 17:20:45 +0530

    fixes

--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/app/Hero/Section1"
 import EventsSection from "@/components/app/Hero/Section2"
 import CreateEventSection from "@/components/app/Hero/Section3"
 import BrandsSection from "@/components/app/Hero/Section4"
@@ -7,11 +8,13 @@ import FAQ from "@/components/Faq"
 function Hero() {
   return (
     <div>
+      <HeroSection />
       <div className="mx-auto px-4 py-12 space-y-20">
         <EventsSection />
         <CreateEventSection />
         <BrandsSection />
         <CollegesSection />
+
         <FAQ />
       </div>
     </div>
