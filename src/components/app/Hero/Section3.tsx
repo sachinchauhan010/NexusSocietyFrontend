51854a30ce29b7e8c2eeeeede08ea033dbe24a32
Aabid Ali <mdaabidali28@gmail.com>
2025-04-15 00:55:39 +0530

    home section1 & 3

--- src/components/app/Hero/Section3.tsx
+++ src/components/app/Hero/Section3.tsx
@@ -1,26 +1,44 @@
 import { Button } from "@/components/ui/button"
-import WebBanner from '/Webbanner.jpg'
+import { CalendarPlus } from "lucide-react";
 
 export default function CreateEventSection() {
   return (
-    <section className="bg-indigo-900 rounded-lg overflow-hidden">
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-5/12 p-8 flex items-center justify-center">
-          <img
-            src={WebBanner}
-            alt="People creating an event"
-            className="max-w-full h-auto"
-          />
-        </div>
-        <div className="md:w-1/2 p-8 flex flex-col justify-center text-white">
-          <h2 className="text-2xl font-bold mb-4">Make your own Event</h2>
-          <p className="text-white/80 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <div>
-            <Button className="bg-purple-600 hover:bg-purple-700">Create Events</Button>
+    <div>
+      <div>
+        <h2 className="text-3xl font-bold mb-6">
+          Be The <span className="text-purple-600">Organizer</span>
+        </h2>
+      </div>
+
+      <section className="bg-purple-300 rounded-lg overflow-hidden">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-5/12 p-8 flex items-center justify-center">
+            <img
+              src="./createevent.jpg"
+              alt="People creating an event"
+              className="max-w-full h-auto"
+            />
+          </div>
+          <div className="md:w-1/2 p-8 flex flex-col justify-center text-white">
+            <h2 className="text-3xl font-bold mb-4">Create Your Own Event</h2>
+            <p className="text-white mb-6 font-medium text-lg">
+              Be more than just a resident — be a creator of memories. Plan your
+              own events that bring people closer, celebrate diversity, and
+              build unity. From festive functions to meaningful meetups, make it
+              all happen in just a few clicks. Let your ideas shine and let the
+              society celebrate with you. Because great communities are built
+              through shared moments
+            </p>
+            <div>
+              <Button className="bg-purple-600 hover:bg-purple-700 flex items-center gap-2">
+                <CalendarPlus className="w-5 h-5 font-semibold " />
+                Create Event
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    </div>
+  );
 }
 
