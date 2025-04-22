10a86c8f5d27fd7a33ed1a40a5b1f454c65b93c8
Sachin <010sssachin@gmail.com>
2025-04-23 00:48:09 +0530

    order created

--- src/components/app/Product/ProductDetail.tsx
+++ src/components/app/Product/ProductDetail.tsx
@@ -3,12 +3,17 @@ import { useParams } from "react-router-dom";
 import { ProductType } from "@/types/productType";
 import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
+import { useAuth } from "@/context/AuthContext";
+import { toast } from "sonner";
+import { OrderType } from "@/types/orderType";
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState<ProductType | null>(null);
+  const [order, setOrder] = useState<OrderType | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const { authState } = useAuth();
 
   useEffect(() => {
     const fetchProductDetail = async () => {
@@ -40,6 +45,63 @@ function ProductDetail() {
     }
   }, [id]);
 
+  const handleOrder = async () => {
+    if (!authState?.email) {
+      alert("Please log in to place an order.");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_PRODUCTION_API_URI}/api/order/create-order`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            products: [
+              {
+                productId: product?.id,
+                name: product?.name,
+                price: product?.price,
+                quantity: 1,
+                image: product?.image,
+              }
+            ],
+            user: authState.email,
+            paymentInfo: {
+              method: "COD",
+              status: "Pending",
+            },
+            totalAmount: product?.price,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to place order");
+      }
+
+      const data = await response.json();
+      setOrder(data.orderData);
+      toast.message("Order is being processed...");
+
+      if(data.paymentInfo.status !== "Pending") {
+        toast.message("please Payment for place the Order")
+      }
+
+      if(data.paymentInfo.status === "Success") {
+        toast.success("Order placed successfully!");
+      }
+
+    } catch (err) {
+      console.error("Error placing order:", err);
+      toast.error("Failed to place order. Please try again later.");
+    }
+  };
+
   if (loading) {
     return (
       <div className="min-h-[60vh] flex items-center justify-center text-lg text-gray-600">
@@ -47,6 +109,7 @@ function ProductDetail() {
       </div>
     );
   }
+  console.log(order, "order data");
 
   if (error) {
     return (
@@ -101,7 +164,10 @@ function ProductDetail() {
             </p>
           </div>
 
-          <button className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg font-medium transition flex items-center gap-2">
+          <button
+            className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg font-medium transition flex items-center gap-2"
+            onClick={handleOrder}
+          >
             <ShoppingCart className="w-5 h-5" />
             Buy Now
           </button>
