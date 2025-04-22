0e8e619be67fa5dae2e8363cd2996afbc768bb5b
Sachin <010sssachin@gmail.com>
2025-04-22 10:13:50 +0530

    event detail loader is added

--- src/page/EventDetails.tsx
+++ src/page/EventDetails.tsx
@@ -4,10 +4,13 @@ import Section3 from "@/components/app/events/Section3";
 import { EventType } from "@/types/eventType";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loader from "@/components/app/Loader";
+
 
 function UserEvent() {
-  const { id } = useParams<{ id: string }>(); // Get the event ID from the URL
-  const [event, setEvent] = useState<EventType | null>(null); // Store a single event
+  const { id } = useParams<{ id: string }>(); 
+  const [event, setEvent] = useState<EventType | null>(null); 
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     if (id) {
@@ -17,6 +20,7 @@ function UserEvent() {
 
   const fetchEvent = async () => {
     try {
+      setLoading(true)
       const response = await fetch(
         `${import.meta.env.VITE_PRODUCTION_API_URI}/api/event/${id}`, // Use the event ID in the API URL
         {
@@ -34,12 +38,19 @@ function UserEvent() {
       setEvent(apiData.eventData || null); // Set the fetched event data
     } catch (error) {
       console.error("Error fetching event:", error);
+    } finally{
+      setLoading(false)
     }
   };
 
-  if (!event) {
-    return <p className="text-center text-gray-500">Loading event details...</p>;
+  if (loading || !event) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader />
+      </div>
+    );
   }
+
 
   return (
     <div className="min-h-screen">
